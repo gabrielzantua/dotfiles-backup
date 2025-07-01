bd860c5dc65784abcbc677844e15b93c62cb147a
gabrielzantua <gabrielzantua1412@gmail.com>
2025-07-02 01:52:02 +0800

    Backup: 2025-07-02 01:52:02

--- .config/Windsurf/User/History/-2bb940ed/mBvj.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# -----------------------------------------------------------------------------
-# backup_to_github.sh
-# -----------------------------------------------------------------------------
-# A simple Bash script that backs up a set of personal files & directories to a
-# GitHub repository. It copies the configured paths from your $HOME directory
-# into a local git repository, then commits & pushes any cha
